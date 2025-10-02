b70b41330ba60c6e316e1d16b4c367bcf53b6826
Chia168168 <chia168168@gmail.com>
2025-10-02 17:30:15 +0800

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Recipe(db.Model):
     __tablename__ = 'recipes'
     
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False, unique=True)
     steps = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     top_heat = db.Column(db.Integer, default=200)
